b2753ca7934224ad6a2870f17da456b45d24899d
gwanyong <78327961+gwanyong@users.noreply.github.com>
2021-05-11 16:24:00 +0900

    JS : menu scrollinToview

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 'use strict';
 
+// navbar
 const navbar = document.querySelector('#navbar');
+
 const navbarHeight = navbar.getBoundingClientRect().height;
 
+const menu = document.querySelector('.navbar__menu__item');
+
 document.addEventListener('scroll', () => {
-  console.log(navbar.getBoundingClientRect());
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar__dark');
   } else {
     navbar.classList.remove('navbar__dark');
   }
+});
+
+document.addEventListener('click', (e) => {
+  const linkname = e.target.dataset.name;
+  const link = document.querySelector('#' + linkname);
+  link.scrollIntoView({ behavior: 'smooth', block: 'center' });
 });
