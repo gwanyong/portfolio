3c31abe5854afd3bdea9f8eab07d298f7ad18af8
gwanyong <78327961+gwanyong@users.noreply.github.com>
2021-05-11 18:21:59 +0900

    JS : home fade out

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
 // navbar
 const navbar = document.querySelector('#navbar');
+const home = document.querySelector('.home__container');
 
 const navbarHeight = navbar.getBoundingClientRect().height;
-
-const menu = document.querySelector('.navbar__menu__item');
+const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
@@ -20,4 +20,8 @@ document.addEventListener('click', (e) => {
   const linkname = e.target.dataset.name;
   const link = document.querySelector('#' + linkname);
   link.scrollIntoView({ behavior: 'smooth', block: 'center' });
+});
+
+document.addEventListener('scroll', () => {
+  home.style.opacity = 3 - window.scrollY / homeHeight;
 });
